f466db0ab67e9f0df8efba0b9a27be42c7b5571b
Navid <navidahmadi6293@gmail.com>
2025-04-28 23:50:46 +0330

    Update Django settings, recommendations models and FastAPI endpoint

--- django_project/recommendations/models.py
+++ django_project/recommendations/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Movie(models.Model):
+    name = models.CharField(max_length=200)
+    genr = models.ManyToManyField(Genre)
+    imdb_rateing = models.FloatField()
+    release_date = models.DateField()
