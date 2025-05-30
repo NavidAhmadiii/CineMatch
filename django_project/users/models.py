b4a3202288b41747ffd7e9fe115aab71cc60683a
Navid Ahmadi <navidahmadi6293@gmail.com>
2025-05-30 18:32:59 +0330

    add custom user model

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
 # Create your models here.
+class CustomUser(AbstractBaseUser):
+    # add aditional field
+    phone_number = models.CharField(max_length=15, blank=True)
+    date_of_birth = models.DateField(null=True,blank=True)
+    bio = models.TextField(max_length=250 ,blank=True)
